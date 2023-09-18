fcc894e41b1e5c681e998062a0c7808c7b905c33
FerrerasE <143365573+FerrerasE@users.noreply.github.com>
2023-09-18 18:58:28 -0300

    prueba de agregar archivos al branch FerrerasE (cambios en src)

---
+++ src/Producto.js
@@ -0,0 +1,11 @@
+class Producto{
+    constructor(nombre,precio,sucursal){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.sucursal = sucursal;
+
+    }
+    
+    
+}
+export default Producto;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import producto from "./Producto.js";
+let productos = [];
+document.getElementById("").onclick = () =>{
+    let productoUno = new producto (
+
+    )
+
+}
+
